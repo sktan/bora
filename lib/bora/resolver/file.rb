7d324428e0c602651972bbe581c37a9431673ba6
Steven Tan <steven.tan@bulletproof.net>
2019-07-23 13:42:42 +1000

    Added file resolver to read a file

---
+++ lib/bora/resolver/file.rb
@@ -0,0 +1,25 @@
+require 'bora/cfn/stack'
+
+class Bora
+  module Resolver
+    class File
+      FileDoesntExist = Class.new(StandardError)
+      InvalidParameter = Class.new(StandardError)
+
+      def initialize(stack)
+        @stack = stack
+        @stack_cache = {}
+      end
+
+      def resolve(uri)
+        file_contents = nil
+        if File.file?(uri.query)
+            file_contents = File.read(uri.query)
+        else
+            raise FileDoesntExist, "Could not load file #{uri.query} for reading"
+        end
+        file_contents
+      end
+    end
+  end
+end
